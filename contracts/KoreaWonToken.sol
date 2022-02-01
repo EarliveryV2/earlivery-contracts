34cb552033a23ada571f7446519c278dcf6c448b
elHongsh <elHongsh@gmail.com>
2022-02-01 22:43:43 +0900

    chore : Add openzeppelin/contracts version 3.4.2 and update solc to 0.6.12
    Due to library importing issue in Truffle, project installs @openzeppelin/contracts@3.4.2 to compile contracts. (Contracts were successfully compiled from Remix)

--- contracts/KoreaWonToken.sol
+++ contracts/KoreaWonToken.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.6.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract KoreaWonToken is ERC20 {
     constructor() ERC20("korea-thousand-won", "KRTW") public {
